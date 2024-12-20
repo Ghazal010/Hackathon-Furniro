9cfd289292ba6fd0a5d6c9f3807fa8e61a04ae3c
Ghazal Shaikh <159722197+Ghazal010@users.noreply.github.com>
2024-12-20 17:17:27 +0500

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,39 +7,58 @@ import Link from "next/link";
 
 const Header = () => {
     return (
-        <div className="bg-white relative h-24 w-screen flex justify-center items-center">
-            <span className="absolute top-4 left-4 flex items-center">
+        <div className="bg-white relative h-24 w-screen flex items-center justify-between px-4 md:px-8 lg:px-16 shadow-md">
+            
+            <div className="flex items-center">
                 <Image
                     src="/logo.png"
                     alt="Furniro Logo"
-                    width={96}
-                    height={96}
+                    width={48}
+                    height={48}
                     priority
                 />
-                <span className="font-sans font-extrabold text-5xl ml-4">Furniro</span>
-            </span>
-            <span>
-                <nav className="hidden md:flex gap-x-20 text-black font-semibold absolute top-16 left-[460px]">
-                    <Link href="/" passHref>
-                        Home
-                    </Link>
-                    <Link href="./shop" passHref>
-                        Shop
-                    </Link>
-                    <Link href="./blog">
-                        Blog
-                    </Link>
-                    <Link href="./contact" passHref>
-                        Contact
-                    </Link>
-                </nav>
-            </span>
-            <span className="flex absolute right-[120px] text-2xl top-16 gap-x-12">
-                <BsPerson />
-                <FiSearch />
-                <FaRegHeart />
-                <AiOutlineShoppingCart />
-            </span>
+                <span className="font-sans font-extrabold text-2xl md:text-3xl ml-4">Furniro</span>
+            </div>
+
+           
+            <nav className="hidden md:flex gap-x-6 lg:gap-x-12 font-medium text-gray-700">
+                <Link href="/" passHref>
+                    Home
+                </Link>
+                <Link href="./shop" passHref>
+                    Shop
+                </Link>
+                <Link href="./blog" passHref>
+                    Blog
+                </Link>
+                <Link href="./contact" passHref>
+                    Contact
+                </Link>
+            </nav>
+
+            
+            <div className="flex gap-x-4 md:gap-x-6 text-gray-600 text-xl md:text-2xl">
+                <BsPerson className="cursor-pointer" />
+                <FiSearch className="cursor-pointer" />
+                <FaRegHeart className="cursor-pointer" />
+                <AiOutlineShoppingCart className="cursor-pointer" />
+            </div>
+
+           
+            <div className="md:hidden flex flex-col items-center space-y-2 absolute top-16 left-0 w-full bg-white shadow-lg p-4">
+                <Link href="/" passHref>
+                    Home
+                </Link>
+                <Link href="./shop" passHref>
+                    Shop
+                </Link>
+                <Link href="./blog" passHref>
+                    Blog
+                </Link>
+                <Link href="./contact" passHref>
+                    Contact
+                </Link>
+            </div>
         </div>
     );
 };
